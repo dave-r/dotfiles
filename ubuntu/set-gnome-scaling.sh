67ee37591e095eb7a78de50885b4e5c869900ee0
Dave R <dave@rouser.org>
2015-12-14 09:37:05 -0500

    added script for setting scaling in gnome

---
+++ ubuntu/set-gnome-scaling.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gsettings set com.ubuntu.user-interface scale-factor "{'eDP1': 12}"
+gsettings set com.canonical.Unity.Interface text-scale-factor '1.25'
+gsettings set org.gnome.desktop.interface text-scaling-factor '1.25'
