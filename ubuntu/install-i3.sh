4db979f100200446e83ee2e731e3c35d9b30f5da
dave-r <dave@rouser.org>
2016-08-29 21:25:12 -0400

    Make install script call app install scripts

---
+++ ubuntu/install-i3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list.d/i3.list
+apt-get update
+apt-get --allow-unauthenticated install sur5r-keyring
+apt-get update
+apt-get install i3
+apt-get install xinit
