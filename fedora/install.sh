e0383e643085c84f92bb8d2240a3f788a294a333
Dave R <dave@rouser.org>
2016-09-06 14:42:50 -0400

    install dot files (.bashrc, .dircolors, ...)

--- fedora/install.sh
+++ fedora/install.sh
@@ -3,3 +3,53 @@
 # get the directory where the script is located
 SCRIPT_DIR=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
 
+# get the user name (not the root user running the script)
+username=""
+. $SCRIPT_DIR/getusername.sh
+getuser
+
+HOMEDIR="/home/$username"
+
+# link bash dot files
+if [[ -f $SCRIPT_DIR/.bashrc ]]
+    then
+        [[ -f $HOMEDIR/.bashrc ]] && mv $HOMEDIR/.bashrc $HOMEDIR/.backup-bashrc-backup
+        ln -s $SCRIPT_DIR/.bashrc $HOMEDIR/.bashrc
+        chmod 700 $HOMEDIR/.bashrc
+fi
+
+if [[ -f $SCRIPT_DIR/.bash_aliases ]]
+    then
+        [[ -f $HOMEDIR/.bash_aliases ]] && mv $HOMEDIR/.bash_aliases $HOMEDIR/.backup-bash_aliases-backup
+        ln -s $SCRIPT_DIR/.bash_aliases $HOMEDIR/.bash_aliases  
+        chmod 700 $HOMEDIR/.bash_aliases      
+fi
+
+if [[ -f $SCRIPT_DIR/.Xresources ]]
+    then 
+        [[ -f $HOMEDIR/.Xresources ]] && mv $HOMEDIR/.Xresources $HOMEDIR/.backup-xresources-backup
+        ln -s $SCRIPT_DIR/.Xresources $HOMEDIR/.Xresources
+        chmod 700 $HOMEDIR/.Xresources
+fi
+
+# install fonts
+if [[ -d $SCRIPT_DIR/../fonts ]]
+    then
+        [[ ! -d $HOMEDIR/.fonts ]] && mkdir $HOMEDIR/.fonts
+        cp -r $SCRIPT_DIR/../fonts/* $HOMEDIR/.fonts/
+fi
+
+# set up dir colors
+if [[ -f $SCRIPT_DIR/../.dircolors ]]
+    then
+        [[ -f $HOMEDIR/.dircolors ]] && mv $HOMEDIR/.dircolors $HOMEDIR/.backup-dircolors-backup
+        cp $SCRIPT_DIR/../.dircolors $HOMEDIR/
+fi
+
+# set up editor config
+if [[ -f $SCRIPT_DIR/../.editorconfig ]]
+then
+    [[ -f $HOMEDIR/.editorconfig ]] && mv $HOMEDIR/.editorconfig $HOMEDIR/.backup-editorconfig-backup
+    ln -s $SCRIPT_DIR/../.editorconfig $HOMEDIR/.editorconfig
+fi
+
