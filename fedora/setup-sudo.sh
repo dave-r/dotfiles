f408ecc8f65707a8279335aa070f285a55bea1ae
Dave <dave@rouser.org>
2016-09-07 12:14:16 -0400

    setup sudo for fedora

---
+++ fedora/setup-sudo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# get the directory where the script is located
+SCRIPT_DIR=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
+
+# get the user name (not the root user running the script)
+username=""
+. $SCRIPT_DIR/getusername.sh
+getuser
+
+HOMEDIR="/home/$username"
+
+setup_sudo() {
+	# add user to sudoers
+	adduser "$username" sudo
+
+	# add user to systemd groups
+	# then you wont need sudo to view logs and shit
+	gpasswd -a "$username" systemd-journal
+	gpasswd -a "$username" systemd-network
+
+	# add go path to secure path
+	{ \
+		echo -e 'Defaults	secure_path="/usr/local/go/bin:/home/$USERNAME/.go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'; \
+		echo -e 'Defaults	env_keep += "ftp_proxy http_proxy https_proxy no_proxy GOPATH EDITOR"'; \
+		echo -e "${username} ALL=(ALL) NOPASSWD:ALL"; \
+		echo -e "${username} ALL=NOPASSWD: /bin/mount, /sbin/mount.nfs, /bin/umount, /sbin/umount.nfs, /sbin/ifconfig, /sbin/ifup, /sbin/ifdown, /sbin/ifquery"; \
+	} >> /etc/sudoers
+}
+
+setup_sudo
