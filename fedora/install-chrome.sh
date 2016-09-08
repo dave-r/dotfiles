43b4b45367214056d96828a84edaf3ceb644cda4
Dave R <dave@rouser.org>
2016-09-07 22:03:34 -0400

    install chrome

---
+++ fedora/install-chrome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# get the directory where the script is located
+SCRIPT_DIR=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
+
+[[ -f $SCRIPT_DIR/google-chrome.repo ]] && cp $SCRIPT_DIR/google-chrome.repo /etc/yum.repos.d/
+dnf -y install google-chrome-stable
