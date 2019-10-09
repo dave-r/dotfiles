8ad5eb4a12b62152bd529bef078e87f6b62f4991
Dave R <dave@rouser.org>
2019-10-09 15:46:20 -0400

    streamline mac install script. Add mac README

---
+++ mac/README.md
@@ -0,0 +1,8 @@
+# mac install README
+
+## todo: 
+* install git
+* clone repo
+* install xcode (already installed?)
+* install Ruby (already installed?)
+* install homebrew
--- mac/install.sh
+++ mac/install.sh
@@ -7,26 +7,10 @@ cd $HOME
 # 1st, backup existing dotfiles
 #tar -cf $HOME/dotfiles-backup.tar $HOME/.bashrc $HOME/.bash_profile $HOME/.dockerrc $HOME/.gitconfig
 
-# remove existing dotfiles
-[[ -f $HOME/.bashrc ]] && rm -f $HOME/.bashrc
-[[ -f $HOME/.bash_profile ]] && rm -f $HOME/.bash_profile
-[[ -f $HOME/.dockerrc ]] && rm -f $HOME/.dockerrc
-[[ -f $HOME/.coreosrc ]] && rm -f $HOME/.coreosrc
-[[ -f $HOME/.gitconfig ]] && rm -f $HOME/.gitconfig
-[[ -f $HOME/.gorc ]] && rm -f $HOME/.gorc
-
-# link dotfiles
-[[ -f $SCRIPT_DIR/.bashrc ]] && ln -s $SCRIPT_DIR/.bashrc $HOME/.bashrc
-[[ -f $SCRIPT_DIR/.bash_profile ]] && ln -s $SCRIPT_DIR/.bash_profile $HOME/.bash_profile
-[[ -f $SCRIPT_DIR/.dockerrc ]] && ln -s $SCRIPT_DIR/.dockerrc $HOME/.dockerrc
-[[ -f $SCRIPT_DIR/.coreosrc ]] && ln -s $SCRIPT_DIR/.coreosrc $HOME/.coreosrc
-[[ -f $SCRIPT_DIR/../git/.gitconfig ]] && ln -s $SCRIPT_DIR/../git/.gitconfig $HOME/.gitconfig
-[[ -f $SCRIPT_DIR/.gorc ]] && ln -s $SCRIPT_DIR/.gorc $HOME/.gorc
-
-# set permissions
-[[ -f $SCRIPT_DIR/.bashrc ]] && chmod 700 $HOME/.bashrc
-[[ -f $SCRIPT_DIR/.bash_profile ]] && chmod 700 $HOME/.bash_profile
-[[ -f $SCRIPT_DIR/.dockerrc ]] && chmod 700 $HOME/.dockerrc
-[[ -f $SCRIPT_DIR/.coreosrc ]] && chmod 700 $HOME/.coreosrc
-[[ -f $SCRIPT_DIR/../git/.gitconfig ]] && chmod 700 $HOME/.gitconfig
-[[ -f $SCRIPT_DIR/.gorc ]] && chmod 700 $HOME/.gorc
+# remove existing dotfiles, link new dotfiles from this repo and set permissions 
+[[ -f $HOME/.bashrc ]] && rm -f $HOME/.bashrc && ln -s $SCRIPT_DIR/.bashrc $HOME/.bashrc && chmod 700 $HOME/.bashrc
+[[ -f $HOME/.bash_profile ]] && rm -f $HOME/.bash_profile && ln -s $SCRIPT_DIR/.bash_profile $HOME/.bash_profile && chmod 700 $HOME/.bash_profile
+[[ -f $HOME/.dockerrc ]] && rm -f $HOME/.dockerrc && ln -s $SCRIPT_DIR/.dockerrc $HOME/.dockerrc && chmod 700 $HOME/.dockerrc
+[[ -f $HOME/.coreosrc ]] && rm -f $HOME/.coreosrc && ln -s $SCRIPT_DIR/.coreosrc $HOME/.coreosrc && chmod 700 $HOME/.coreosrc
+[[ -f $HOME/.gitconfig ]] && rm -f $HOME/.gitconfig && ln -s $SCRIPT_DIR/../git/.gitconfig $HOME/.gitconfig && chmod 700 $HOME/.gitconfig
+[[ -f $HOME/.gorc ]] && rm -f $HOME/.gorc && ln -s $SCRIPT_DIR/.gorc $HOME/.gorc && chmod 700 $HOME/.gorc
